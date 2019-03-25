083f4e7f88e2359062b5c996a53a5e23b2ef392c
Dmitry Lytov <dmitrycmc@gmail.com>
2019-03-25 11:37:22 +0300

    improved interface

--- user-interface/MainWindow.xaml.cs
+++ user-interface/MainWindow.xaml.cs
@@ -69,28 +69,81 @@ namespace user_interface
 			textBlockrRes.Text = wayName + " squared error: " + sde0.GetAverageSquaredError() + Environment.NewLine;
 
 			var exactSol = sde0.getSolution;
-			plot.drawLine(wayName + " exact solution", exactSol);
+			plot.drawLine(wayName + " exact sol", exactSol);
 
-			plot.drawPoints("Equilibrium point 0", sde0.GetEquilibriumPoint());
+			plot.drawPoints("Exact eq", sde0.GetEquilibriumPoint());
 
 			plot.drawPoints("Initial point", new double[] { sde0.x0, sde0.y0 });
 
 			measurements = Generator.getMeasurements(exactSol, stdDev, n);
-			plot.drawPoints("Measurements", measurements);
+			plot.drawPoints("Meas", measurements);
 		}
 
-		private void infer(object sender, RoutedEventArgs e)
+		private string printParamReport(double val0, double val1, string name)
 		{
+			string res = "";
+			res += name + " origin: " + val0 + Environment.NewLine;
+			res += name + " predicted: " + val1 + Environment.NewLine;
+			res += name + " abs error: " + (val1 - val0) + Environment.NewLine;
+			res += name + " rel error: " + (val1 - val0) / val0 + Environment.NewLine;
+			return res;
+		}
+
+		private string printParamsReport(SDE sde0, SDE sde1, bool myWay)
+		{
+			string res = "";
+			double alpha0 = sde0.alpha, beta0 = sde0.beta, gamma0 = sde0.gamma, delta0 = sde0.delta;
+			double alpha1 = sde1.alpha, beta1 = sde1.beta, gamma1 = sde1.gamma, delta1 = sde1.delta;
+
+			if (myWay)
+			{
+				beta0 /= alpha0;
+				gamma0 /= alpha0;
+				delta0 /= alpha0;
+				alpha0 = 1;
+
+				beta1 /= alpha1;
+				gamma1 /= alpha1;
+				delta1 /= alpha1;
+				alpha1 = 1;
+			}
+			else
+			{
+
+				res += printParamReport(alpha0, alpha1, "Alpha");
+			}
+			res += printParamReport(beta0, beta1, "Beta");
+			res += printParamReport(gamma0, gamma1, "Gamma");
+			res += printParamReport(delta0, delta1, "Delta");
+			double sqerror = Math.Sqrt((
+					Math.Pow(alpha1 - alpha0, 2) +
+					Math.Pow(beta1 - beta0, 2) +
+					Math.Pow(gamma1 - gamma0, 2) +
+					Math.Pow(delta1 - delta0, 2)
+				) / (myWay ? 3 : 4));
+
+			res += "Squared error: " + sqerror;
+
+			return res;
+
+		}
+
+		private void Infer(object sender, RoutedEventArgs e)
+		{
+			bool myWay = (bool)checkBoxMyWay.IsChecked;
 			try
 			{
-				var a = Model.FirstIntegralInfer(measurements);
-				sde1 = new SDE(a[0], a[1], a[2], a[3], a[4]);
+				var inferedParams = Model.FirstIntegralInfer(measurements);
+				sde1 = new SDE(inferedParams[0], inferedParams[1], inferedParams[2], inferedParams[3], inferedParams[4]);
 
 				sde1.Rays(dt);
 				var predictedSol = sde1.getSolution;
-				plot.drawLine("Infered", predictedSol);
+				plot.drawLine("Infered sol", predictedSol);
 				
-				plot.drawPoints("Equilibrium point 1", sde1.GetEquilibriumPoint());
+				plot.drawPoints("Infered eq 1", sde1.GetEquilibriumPoint());
+				textBlockrRes.Text += printParamsReport(sde0, sde1, myWay);
+
+
 			}
 			catch (Exception)
 			{
