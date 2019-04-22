ed82ce9f9c1a1147a5b8c2e8d52680edda1e3241
Dmitry Lytov <dmitrycmc@gmail.com>
2019-04-22 07:55:28 +0300

    sdf

--- collector/Program.cs
+++ collector/Program.cs
@@ -4,11 +4,16 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Randomizer;
+using Solver;
+
 
 namespace collector
 {
 	class Program
 	{
+		static readonly double dt = 0.01;
+
 		static void WriteToFile(string str)
 		{
 			using (System.IO.StreamWriter file =
@@ -62,14 +67,44 @@ namespace collector
 		{
 			int experimentsNum = ReadInt("number of experiments");
 			int sampleSize = ReadInt("size of sample");
+			double stdDev = ReadDouble("stdDev");
 			double minValue = ReadInt("minValue");
 			double maxValue = ReadInt("maxValue");
-			getRandomParams
+
+			double[] parameters = Generator.getRandomParams(minValue, maxValue);
+			SDE sde0 = new SDE(
+				parameters[0],
+				parameters[1],
+				parameters[2],
+				parameters[3],
+				parameters[4],
+				parameters[5]
+			);
+
+			for (int i = 0; i < experimentsNum; i++) {
+
+
+				sde0.Rays(dt);
+
+				string report = i + ": " + sde0.GetAverageSquaredError() + " ";
+				
+				var exactSol = sde0.getSolution;
+				var measurements = Generator.getMeasurements(exactSol, stdDev, sampleSize);
 
 
 
 
 
+
+				sde0.OSLO(dt);
+
+				exactSol = sde0.getSolution;
+				measurements = Generator.getMeasurements(exactSol, stdDev, sampleSize);
+
+
+
+				WriteToFile(report);
+			}
 
 			Console.ReadKey();
 		}
